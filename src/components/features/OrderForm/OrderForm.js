c48f385e52a60a7d10fdc22ee42cef845acc00f3
Konrad Kostrzanowski <konk99@o2.pl>
2019-08-14 20:40:11 +0200

    Add order

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Row, Col } from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+//import styles from './OrderForm.scss';
+import PropTypes from 'prop-types';
+
+const OrderForm = ({ tripCost, options }) => (
+  <Row>
+    <Col xs={12}>
+      <OrderSummary price={tripCost} options={options} />
+    </Col>
+  </Row>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.node,
+  options: PropTypes.array,
+};
+
+export default OrderForm;
