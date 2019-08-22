5356efa9dd58ce5c9badbce26808588fd2ac0637
Konrad Kostrzanowski <konk99@o2.pl>
2019-08-22 21:04:05 +0200

    Add API request

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,18 +5,54 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import  settings  from '../../../data/settings';
+
+const sendOrder = (options, tripCost, countryCode, tripId, tripName) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options, countryCode));
+
+  const payload = {
+    ...options,
+    totalCost,
+    countryCode,
+    tripId,
+    tripName,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function (response) {
+      return response.json();
+    }).then(function (parsedResponse) {
+    });
+};
 
 const OrderForm = props => (
-  <Row>
-    {pricing.map(option => (
-      <Col key={option.id} md={4}>
-        <OrderOption {...option} currentValue={props.options[option.id]} setOrderOption={props.setOrderOption} />
+  <form>
+    < Row >
+      {pricing.map(option => (
+        <Col key={option.id} md={4}>
+          <OrderOption {...option} currentValue={props.options[option.id]} setOrderOption={props.setOrderOption} />
+        </Col>
+      ))}
+      <Col xs={12}>
+        <OrderSummary price={props.tripCost} options={props.options} />
       </Col>
-    ))}
-    <Col xs={12}>
-      <OrderSummary price={props.tripCost} options={props.options} />
-    </Col>
-  </Row>
+      <Button onClick={()=>sendOrder(props.options,props.tripCost)}>Order now!</Button>
+    </Row>
+  </form>
 );
 
 OrderForm.propTypes = {
